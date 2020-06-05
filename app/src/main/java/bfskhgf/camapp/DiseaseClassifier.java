ce965e019a1904ce551af8c112598a8fe4201bc0
Mahale Shantanu <46351765+mahale-shantanu-26@users.noreply.github.com>
2020-06-05 16:28:12 +0530

    updated classifier

--- app/src/main/java/bfskhgf/camapp/DiseaseClassifier.java
+++ app/src/main/java/bfskhgf/camapp/DiseaseClassifier.java
@@ -12,18 +12,18 @@ public class DiseaseClassifier extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_disease_classifier);
 
-//
-//        TensorFlowInferenceInterface tensorflow = new TensorFlowInferenceInterface();
-//        tensorflow.initializeTensorFlow(getAssets(), "file:///android_asset/model.pb");
-//
-//
-//// loading new input
-//        tensorflow.fillNodeFloat("input:0", INPUT_SHAPE, input);
-//
-//// running inference for given input and reading output
-//        String outputNode = "output:0";
-//        String[] outputNodes = {outputNode};
-//        tensorflow.runInference(outputNodes);
-//        tensorflow.readNodeFloat(outputNode, output);
+
+       TensorFlowInferenceInterface tensorflow = new TensorFlowInferenceInterface();
+       tensorflow.initializeTensorFlow(getAssets(), "file:///android_asset/model.pb");
+
+
+// loading new input
+       tensorflow.fillNodeFloat("input:0", INPUT_SHAPE, input);
+
+// running inference for given input and reading output
+       String outputNode = "output:0";
+       String[] outputNodes = {outputNode};
+       tensorflow.runInference(outputNodes);
+       tensorflow.readNodeFloat(outputNode, output);
   }
 }
